ea470fcecdd80de7c7bde5f231e839825d3c7a59
a1015032205 <1015032205@qq.com>
2020-11-19 00:13:43 +0800

    1111

---
+++ src/java/lang/String.java
@@ -0,0 +1,15 @@
+package java.lang;
+
+/**
+ * @Author: 秒度
+ * @Email: fangxin.md@Gmail.com
+ * @Date: 2020-11-16 21:40
+ * @Description: 自定义
+ */
+
+public class String {
+    public static void main(String[] args) {
+        //在类 java.lang.String 中找不到 main 方法
+        System.out.println("this is my String");
+    }
+}
