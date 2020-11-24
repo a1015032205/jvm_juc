d7c8be70206c1a0342a92fbfdcf61ad8889414f8
a1015032205 <1015032205@qq.com>
2020-11-24 23:09:03 +0800

    juc

--- src/juc/util/CountDownLatchDemo.java
+++ src/juc/util/CountDownLatchDemo.java
@@ -13,6 +13,10 @@ import java.util.concurrent.CountDownLatch;
 public class CountDownLatchDemo {
 
     public static void main(String[] args) throws Exception {
+        /* * CountDownLatch主要有两个方法，当一个或多个线程调用await方法时，这些线程会阻塞。
+         * 其它线程调用countDown方法会将计数器减1(调用countDown方法的线程不会阻塞)，
+         * 当计数器的值变为0时，因await方法阻塞的线程会被唤醒，继续执行。
+         */
         //类似计数器  完成一个线程消耗一个  卡住主线程  不能多也不能少
         CountDownLatch downLatch = new CountDownLatch(6);
         for (int i = 0; i < 6; i++) {
